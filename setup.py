00b6769a0518dfecb994a6a5c950a5df690fa595
Alex Myasoedov <msoedov@gmail.com>
2017-08-13 12:06:36 -0400

    Packaging

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="afk",
+    version="0.1.1",
+    description="A command-line tool called afk which gives you a simple reminder when you get back.",
+    license="MIT",
+    author="Alex Myasoedov",
+    author_email="msoedov@gmail.com",
+    packages=['.'],
+    install_requires=['pyobjc', 'pync', 'fire'],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ]
+)
